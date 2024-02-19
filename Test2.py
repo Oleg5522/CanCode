0db33390187f05f2b0700d15a8fbfc89d2677128
unknown <C:\Users\Work\AppData\Roaming\The Bat! Pwd>
2024-02-19 13:37:08 -0500

    doing some for loops

---
+++ Test2.py
@@ -0,0 +1,47 @@
+length=10
+width=7
+
+perimeter=2*(length+width)
+print("The perimetr is ", perimeter)
+num_one=5
+print(num_one)
+print(type(num_one))
+#String
+fav_color="blue"
+print(fav_color)
+print(type(fav_color))
+#Bool
+technical_errors=True
+print(technical_errors)
+print(type(technical_errors))
+#Float
+num_two = 1.22
+print(num_two)
+print(type(num_two))
+#String
+first_name = "Oleg"
+last_name="Bez"
+full_name=first_name + " " + last_name
+print(full_name)
+#List
+student_grades = [100,95,63,12]
+print(student_grades)
+
+print(type(student_grades))
+
+# for loop
+for s in student_grades:
+    print(s)
+
+
+#Dictionary
+    demographic_info = {"First Name":"Oleg", 
+                        "Last name":"Bez",
+                        "State":"NY"}
+    print(demographic_info)
+    print(type(demographic_info))
+
+    #cast a string to integer
+    my_string = "5"
+
+    new_number = int(my_string)
---
+++ Test3.py
@@ -0,0 +1,28 @@
+#cast a string to integer
+#    my_string = "5"
+#print(type(my_string))
+
+ #   new_number = int(my_string)
+
+#print(new_number)
+#print(type(new_number))
+
+second_num=10
+print(type(second_num))
+
+new_string = str(second_num)
+print(new_string)
+print(type(new_string))
+
+#Colors
+#fav_colors=["blue","green","red", "white"] #List item has 4 elements
+#num_colors=len(fav_colors)
+#print(num_colors)
+
+color = "orange"
+my_count = len(color)
+print(my_count)
+
+for c in color:
+    print(c)
+
