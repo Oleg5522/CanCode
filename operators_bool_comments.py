898cba396afd1abbfb1c8345a01e767dbd2d75b0
unknown <C:\Users\Work\AppData\Roaming\The Bat! Pwd>
2024-02-22 13:54:45 -0500

    different bool

--- operators_bool_comments.py
+++ operators_bool_comments.py
@@ -12,3 +12,63 @@ print(year)
 num_one = 55
 num_one -=20
 print(num_one)
+
+# Raise to the 3rd power **
+num_three = 6
+num_three **=3
+print(num_three)
+
+# Integer divide by 4 //
+val_two = 9
+val_two //=4
+print(val_two)
+
+# Find the reminder if divided by 5 %
+val_four = 14
+val_four %=5
+print(val_four)
+
+# Is 7 less than 5 ? <
+print(7<5)
+result = (7<5)
+print(result)
+
+#Is 5 equal to 5? ==
+print(5==5)
+result = (5==5)
+print(result)
+
+#Is 100 not equal 75 ? !=
+print(100!=75)
+result=(100!=75)
+print(result)
+
+# and
+print(5==5 and 4==6)
+
+#or
+print(5==5 or 5==3)
+
+# not
+print("not")
+x = 5
+y = 7
+
+print(x<y)
+print(not x<y)
+
+# Are we the same object? is
+print("The same")
+fname = "Taylor"
+new_name = fname
+print(fname is new_name)
+
+# in
+print("J" in "January")
+
+#Eval
+is_open = "True"
+weekday = "False"
+print(eval(is_open))
+print(eval(weekday))
+
